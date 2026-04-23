531c3244f10ebdfc9a87e2ed32e29ec6c391fc4e
Adrian Valentin <adri_an_04@hotmail.com>
2026-04-23 09:28:13 -0600

    prueba flex

--- src/Clases/Cuarto.java
+++ src/Clases/Cuarto.java
@@ -1,3 +1,5 @@
+package Clases;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +9,6 @@
  *
  * @author Luisf
  */
-public class Mega {
+public class Cuarto {
     
 }
